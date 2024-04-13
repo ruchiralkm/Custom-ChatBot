8c49723dffe18ff132302f79649f4113e92b7e15
Ruchira Kaluarachchi <151660406+ruchiralkm@users.noreply.github.com>
2024-04-13 23:35:24 +0530

    Update responses.js

--- responses.js
+++ responses.js
@@ -1,20 +1,41 @@
 let userName = "";
 
 function getBotResponse(input) {
+  //["hi", "hello"]
+  const greetings = [
+    "hi",
+    "hello",
+    "hey",
+    "hey hey",
+    "whatsup",
+    "hii",
+    "hello hi",
+    "hi hello",
+  ];
+  const responses = [
+    "Whatsup bro<br><img src='asset/emote.gif' alt='' width='50' height='50'>",
+    "Hello dear<br><img src='asset/emote.gif' alt='' width='50' height='50'>",
+    "Hello, Nice to meet you...<br><img src='asset/emote.gif' alt='' width='50' height='50'>",
+  ];
+  const OkResponses = ["Cool😎", "Ok dear👍", "Good dear👍"];
+
   // Simple responses
-  if (input.toLowerCase().includes("hi")) {
-    return "Good";
-  } else if (input.toLowerCase().includes("age")) {
-    return "I am 22.";
-  } else if (input.toLowerCase().includes("name")) {
-    userName = input;
-    return "I am Eva, What is your name?";
-  } else if (!userName) {
+  if (!userName) {
     userName = input; // Set user's name
-    return "Hello " + userName;
-  } else if (input.toLowerCase().includes("bye")) {
-    return `Good bye ${userName}`;
-  } else {
-    return "Invalid";
+    return (
+      "Hello " +
+      userName +
+      ", Welcome to the Brew Cafe...😊. How can I help you today?"
+    );
+  } else if (input == "❤️") {
+    return "<img src='asset/heart2.gif' alt='' width='50' height='50'>";
+  } else if (input.toLowerCase().includes("ok")) {
+    return OkResponses[Math.floor(Math.random() * responses.length)];
+  } else if (input.toLowerCase().includes("menu")) {
+    return (
+      "<img src='asset/mglogo.jpg' alt='' width='250' height='250'<br><br>Hello " +
+      userName +
+      ", Welcome to the Brew Cafe<br>This is our menu. If you want know more details, Please mention food name.<br><br>1.Burgers<br><br>2.Coffee<br><br>3.Pizza"
+    );
   }
 }
